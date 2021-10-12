2d1a6d4b214d776596d16f8d6106f77ceb676720
= <=>
2021-10-13 00:07:52 +0530

    Added a fix so it can't respond to bots

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -3,13 +3,15 @@ const Event = require("../../structures/Event");
 module.exports = class messageCreate extends Event {
   async run(message) {
     try {
+      if (!message.guild || message.author.bot) return;
       if (
         message.mentions.users.has(this.bot.user.id) &&
         message.content === `<@!${this.bot.user.id}>`
-      )
-        return message.channel.send(`Hi, I only listen to slash commands!`);
-
-      if (!message.guild || message.author.bot) return;
+      ) {
+        return message.channel.send(
+          "Hi, I only listen to slash commands! Type `/activity`"
+        );
+      }
     } catch (error) {
       console.error(error);
     }
